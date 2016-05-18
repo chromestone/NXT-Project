1e4a0cec8237fd43dd1fcc0ec4dff44728224570
Derek Zhang <derekzhang@Dereks-MBP.home>
2016-05-18 17:20:19 -0400

    Main code added

---
+++ TCP Client/src/com/github/chromestone/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.github.chromestone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.chromestone.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 400;
+		config.height = 400;
+		new LwjglApplication(new Application(), config);
+	}
+}
