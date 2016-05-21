e3a12250d08a7216b3cdc6f4bcd10e9570520b74
Derek Zhang <derekzhang@Dereks-MBP.home>
2016-05-21 16:44:11 -0400

    Revert "Comments"
    This reverts commit 7cde9c958cec6f26be2e8f2b1b82e3d5de963a6e.

--- TCP Client/src/com/github/chromestone/desktop/DesktopLauncher.java
+++ TCP Client/src/com/github/chromestone/desktop/DesktopLauncher.java
@@ -2,15 +2,8 @@ package com.github.chromestone.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import com.github.chromestone.Application;
 
-/**
- * Contains the main method that launches the "application"
- * 
- * @author Derek Zhang
- * 
- */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
